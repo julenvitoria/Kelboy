4886a9f303e012a0a8417cb6d48ccc17b897fe00
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2020-11-14 20:07:37 +0100

    restoresystems.sh

--- patchs/restoresystems.sh
+++ patchs/restoresystems.sh
@@ -5,17 +5,17 @@ echo "Aun asi se puede usar... ;)"
 sleep 5
 
 #Create /roms/kelboy directory and download files
-if [ -d /home/pi/RetroPie/roms/kelboy/ ]; then
-        echo "Directory /roms/kelboy was created yet. Downloading kelboy system..."
-        wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/kelboy/kelboy.sh>/home/pi/RetroPie/roms/kelboy/kelboy.sh
-        chmod +x /home/pi/RetroPie/roms/kelboy/kelboy.sh
+if [ -d /home/pi/scripts/kelboy/ ]; then
+        echo "Directory /home/pi/scripts/kelboy/ was created yet. Downloading kelboy system..."
+        wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/kelboy/kelboy.sh>/home/pi/scripts/kelboy/kelboy.sh
+        chmod +x /home/pi/scripts/kelboy/kelboy.sh
         sleep 2
 else
         echo "Creating kelboy directory and downloading kelboy system..."
         sleep 2
-        mkdir /home/pi/RetroPie/roms/kelboy/
-        wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/kelboy/kelboy.sh>/home/pi/RetroPie/roms/kelboy/kelboy.sh
-        chmod +x /home/pi/RetroPie/roms/kelboy/kelboy.sh
+        mkdir /home/pi/scripts/kelboy/
+        wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy/main/patchs/kelboy/kelboy.sh>/home/pi/scripts/kelboy/kelboy.sh
+        chmod +x /home/pi/scripts/kelboy/kelboy.sh
         sleep 2
 fi
 
@@ -23,7 +23,7 @@ fi
 if grep -q "<name>kelboy</name>" /etc/emulationstation/es_systems.cfg ; then
         echo "kelboy system already exists, nothing to mod..."
 else
-        sudo sed -i 's|</systemList>|  <system>\n    <name>kelboy</name>\n    <fullname>kelboy</fullname>\n    <path>/home/pi/RetroPie/roms/kelboy</path>\n    <extension>.sh</extension>\n    <command>/home/pi/RetroPie/roms/kelboy/kelboy.sh</command>\n    <platform>kelboy</platform>\n    <theme>kelboy</theme>\n  </system>\n</systemList>|' /etc/emulationstation/es_systems.cfg
+        sudo sed -i 's|</systemList>|  <system>\n    <name>kelboy</name>\n    <fullname>kelboy</fullname>\n    <path>/home/pi/scripts/kelboy</path>\n    <extension>.sh</extension>\n    <command>/home/pi/scripts/kelboy/kelboy.sh</command>\n    <platform>kelboy</platform>\n    <theme>kelboy</theme>\n  </system>\n</systemList>|' /etc/emulationstation/es_systems.cfg
 fi
 
 echo "RestoreSystems fix applied!!!"
