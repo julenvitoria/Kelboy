a59dac2b41cff8518d71041df04aae7db1deb837
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2021-03-12 20:50:08 +0100

    install update and autostartmenu creation

--- patchs/autostartmenu.sh
+++ patchs/autostartmenu.sh
@@ -16,25 +16,37 @@ case $menuitem in
   1)clear
     echo "Configuring boot sequence..."
     sleep 2
+    if [ -f /opt/retropie/configs/all/autostart.sh ]; then
+    rm /opt/retropie/configs/all/autostart.sh
+    fi
+    sleep 2
     touch /opt/retropie/configs/all/autostart.sh
     sed -i '/#auto/d' "/opt/retropie/configs/all/autostart.sh"
-    sed -i '$a\' "/opt/retropie/configs/all/autostart.sh"
+    sed -i '$a\\' "/opt/retropie/configs/all/autostart.sh"
     echo -e "if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then #auto\n    echo \"welcome to kelboy throw ssh session\" #auto\nelse #auto\n    cd kelboy-launcher #auto\n    ./launcher.sh #auto\nfi #auto" > /opt/retropie/configs/all/autostart.sh
-    #clear;;
+    clear;;
   2)clear
     echo "Configuring boot sequence..."
     sleep 2
-    touch /opt/retropie/configs/all/autostart.sh
-    sed -i '/#auto/d' "/opt/retropie/configs/all/autostart.sh"
-    sed -i '$a\' "/opt/retropie/configs/all/autostart.sh"
-    echo "emulationstation #auto"
-    #clear;;
-  3)clear
-    echo "Configuring boot sequence..."
+    if [ -f /opt/retropie/configs/all/autostart.sh ]; then
+    rm /opt/retropie/configs/all/autostart.sh
+    fi
     sleep 2
     touch /opt/retropie/configs/all/autostart.sh
     sed -i '/#auto/d' "/opt/retropie/configs/all/autostart.sh"
-    sed -i '$a\' "/opt/retropie/configs/all/autostart.sh"
-    echo -e "kodi-standalone #auto\nemulationstation #auto"
-    #clear;;
+    sed -i '$a\\' "/opt/retropie/configs/all/autostart.sh"
+    echo -e "cd /home/pi/kelboy-launcher && python3 joystick.py & #auto\\nemulationstation #auto" > /opt/retropie/configs/all/autostart.sh
+    clear;;
+  3)clear
+    echo "Configuring boot sequence..."
+    sleep 2
+    if [ -f /opt/retropie/configs/all/autostart.sh ]; then
+    rm /opt/retropie/configs/all/autostart.sh
+    fi
+    sleep 2
+    touch /opt/retropie/configs/all/autostart.sh
+    sed -i '/#auto/d' "/opt/retropie/configs/all/autostart.sh"
+    sed -i '$a\\' "/opt/retropie/configs/all/autostart.sh"
+    echo -e "cd /home/pi/kelboy-launcher && python3 joystick.py & #auto\\nkodi-standalone #auto\\nemulationstation #auto" > /opt/retropie/configs/all/autostart.sh
+    clear;;
 esac
