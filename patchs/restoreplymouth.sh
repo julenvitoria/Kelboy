7db8b37a5a88605f78126cc7ccc700e76f93b596
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2021-03-16 18:53:27 +0100

    Update restoreplymouth.sh

--- patchs/restoreplymouth.sh
+++ patchs/restoreplymouth.sh
@@ -5,6 +5,7 @@ sleep 1
 cd ~/RetroPie-Setup/
 sudo ./retropie_packages.sh splashscreen disable
 sudo sed -i 's|plymouth.enable=0||' /boot/cmdline.txt
+sudo plymouth-set-default-theme retropie-bar
 
 echo "Fix Applied!!!"
 sleep 2
