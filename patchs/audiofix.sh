c76da5b9b08daef53fe57f9e652c1c992d09f513
julenvitoria <julenvitoria@hotmail.es>
2021-07-10 12:36:42 +0200

    audiofix

--- patchs/audiofix.sh
+++ patchs/audiofix.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #Fix EmulationStation audio settings
-sed -i 's/.*<string name="AudioCard" value=.*/<string name="AudioCard" value="hw" \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
-sed -i 's/.*<string name="AudioDevice" value=.*/<string name="AudioDevice" value="PCM" \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
-sed -i 's/.*<string name="OMXAudioDev" value=.*/<string name="OMXAudioDev" value="alsa" \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+#sed -i 's/.*<string name="AudioCard" value=.*/<string name="AudioCard" value="hw" \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+#sed -i 's/.*<string name="AudioDevice" value=.*/<string name="AudioDevice" value="PCM" \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+#sed -i 's/.*<string name="OMXAudioDev" value=.*/<string name="OMXAudioDev" value="alsa" \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
 #Fix asplashcreen.sh
-sudo sed -i 's/.*omxplayer --no-osd -o both -b.*/omxplayer --no-osd -o alsa -b --layer 10000 "\$line"/' /opt/retropie/supplementary/splashscreen/asplashscreen.sh
+#sudo sed -i 's/.*omxplayer --no-osd -o both -b.*/omxplayer --no-osd -o alsa -b --layer 10000 "\$line"/' /opt/retropie/supplementary/splashscreen/asplashscreen.sh
+sudo raspi-config nonint do_audio "0"
 
 echo "Audio fix applied!!!"
 sleep 3
